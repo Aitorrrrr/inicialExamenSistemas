2ab5c2eb3b63b47e6b4413f61b086cf9090e958f
ALUMNADO\airuso <aitorinfsedavi@gmail.com>
2017-11-17 19:20:31 +0100

    Subo todos los archivos

---
+++ bloque4_Ejercicio10/src/bloque4_Ejercicio10/Principal.java
@@ -0,0 +1,40 @@
+package bloque4_Ejercicio10;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num;
+		String numInverso;
+		Scanner sc=new Scanner(System.in);
+		
+		do
+		{
+			System.out.println("Introduzca un número entero >0 para invertirlo");
+			num=sc.nextInt();
+		}
+		while (num<1);
+		
+		numInverso=numeroInverso(num);
+		
+		System.out.println("El número inverso a "+num+" es: "+numInverso);
+	}
+	
+	public static String numeroInverso(int num)
+	{
+		String numInverso="";
+		int cifra;
+		
+		while (num>0)
+		{
+			cifra=num%10;
+			numInverso=numInverso+cifra;
+			num/=10;
+		}
+		
+		return numInverso;
+	}
+}
